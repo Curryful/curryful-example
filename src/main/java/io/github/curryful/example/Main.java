ca8f05ce63e6d12d62b7425527f0aab8b52bc681
Nico Lerchl <nico@lerchl.one>
2024-02-20 16:19:09 +0100

    initial commit

---
+++ src/main/java/io/github/curryful/example/Main.java
@@ -0,0 +1,28 @@
+package io.github.curryful.example;
+
+import static io.github.curryful.rest.Router.listen;
+
+import java.util.HashMap;
+
+import io.github.curryful.rest.HttpMethod;
+import io.github.curryful.rest.Destination;
+import io.github.curryful.rest.Endpoint;
+import io.github.curryful.rest.HttpResponse;
+import io.github.curryful.rest.HttpResponseCode;
+
+// void main() {
+//     var routes = new HashMap<Destination, Endpoint>();
+//     routes.put(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!"));
+
+//     listen.apply(routes).apply(8080);
+// }
+
+public class Main {
+
+	public static void main(String[] args) {
+		var routes = new HashMap<Destination, Endpoint>();
+		routes.put(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!"));
+		listen.apply(routes).apply(8080);
+	}
+}
+
