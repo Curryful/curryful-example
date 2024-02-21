6ab02f17cbadc49b93fdefeacba678db39ae113e
Nico Lerchl <nico@lerchl.one>
2024-02-21 16:06:48 +0100

    add path parameter endpoint

--- src/main/java/io/github/curryful/example/Main.java
+++ src/main/java/io/github/curryful/example/Main.java
@@ -22,6 +22,7 @@ public class Main {
 	public static void main(String[] args) {
 		var routes = new HashMap<Destination, Endpoint>();
 		routes.put(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!"));
+		routes.put(new Destination(HttpMethod.GET, "/hello/:name"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, " + _context.get("name") + "!"));
 		listen.apply(routes).apply(8080);
 	}
 }
