7962c15291009214b2af9a21f27613302a6db29f
Nico Lerchl <nico@lerchl.one>
2024-02-25 13:03:02 +0100

    use path parameter

--- src/main/java/io/github/curryful/example/Main.java
+++ src/main/java/io/github/curryful/example/Main.java
@@ -2,28 +2,24 @@ package io.github.curryful.example;
 
 import static io.github.curryful.rest.Router.listen;
 
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.List;
 
-import io.github.curryful.rest.HttpMethod;
 import io.github.curryful.rest.Destination;
 import io.github.curryful.rest.Endpoint;
+import io.github.curryful.rest.HttpMethod;
 import io.github.curryful.rest.HttpResponse;
 import io.github.curryful.rest.HttpResponseCode;
-
-// void main() {
-//     var routes = new HashMap<Destination, Endpoint>();
-//     routes.put(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!"));
-
-//     listen.apply(routes).apply(8080);
-// }
 
 public class Main {
 
 	public static void main(String[] args) {
-		var routes = new HashMap<Destination, Endpoint>();
-		routes.put(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!"));
-		routes.put(new Destination(HttpMethod.GET, "/hello/:name"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, " + _context.get("name") + "!"));
-		listen.apply(routes).apply(8080);
+		List<Endpoint> endpoints = Arrays.asList(
+			Endpoint.of(new Destination(HttpMethod.GET, "/hello"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, world!")),
+			Endpoint.of(new Destination(HttpMethod.GET, "/hello/:name"), _context -> new HttpResponse<String>(HttpResponseCode.OK, "Hello, " + _context.getPathParameters().get("name") + "!"))
+		);
+
+		listen.apply(endpoints).apply(8080);
 	}
 }
 
